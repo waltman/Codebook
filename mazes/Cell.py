d609caa32f2090fea05ba4cdf33e14721598c315
Walt Mankowski <waltman@pobox.com>
2019-05-16 09:58:16 -0400

    first draft of Code and Grid classes
    These are pretty much just ports of the code in the book from ruby to python3

---
+++ mazes/Cell.py
@@ -0,0 +1,34 @@
+class Cell:
+    def __init__(self, row, col):
+        self._row = row
+        self._col = col
+        self._links = set()
+        self.north = None
+        self.south = None
+        self.east = None
+        self.west = None
+
+    def link(self, cell, bidi=True):
+        self._links.add(cell)
+        if bidi:
+            cell.link(self, False)
+
+    def unlink(self, cell, bidi=True):
+        self._links.remove(cell)
+        if bidi:
+            cell.unlink(self, False)
+
+    def links(self):
+        return list(self._links)
+
+    def is_linked(self, cell):
+        return cell in self._links
+
+    def neighbors(self):
+        return [x for x in (self.north, self.south, self.east, self.west) if x]
+
+    def __repr__(self):
+        return f'row={self._row}, col={self._col}'
+
+    def __str__(self):
+        return f'({self._row},{self._col})'
---
+++ mazes/Grid.py
@@ -0,0 +1,50 @@
+from random import randint
+
+from Cell import Cell
+
+class Grid:
+    def __init__(self, rows, cols):
+        self._rows = rows
+        self._cols = cols
+        
+        self.prepare_grid()
+        self.configure_cells()
+
+    def __getitem__(self, rc):
+        row, col = rc
+        if not 0 <= row <= self._rows-1:
+            return None
+        if not 0 <= col <= self._cols:
+            return None
+        return self._grid[row,col]
+
+    def each_row(self):
+        for row in self._grid:
+            yield(row)
+
+    def each_cell(self):
+        for row in self.each_row():
+            for cell in row:
+                if cell:
+                    yield(cell)
+
+    def prepare_grid(self):
+        self._grid = [[Cell(row, col) for col in range(self._cols)] for row in range(self._rows)]
+
+    def configure_cells(self):
+        for cell in self.each_cell:
+            row, col = cell
+
+            cell.north = self[row-1, col]
+            cell.south = self[row+1, col]
+            self.west  = self[row, col-1]
+            self.east  = self[row, col+1]
+
+    def random_cell(self):
+        row = randint(0, self._rows-1)
+        col = randint(0, self._cols-1)
+        return self[row, col]
+
+    def __len__(self):
+        return self._rows * self._cols
+    
