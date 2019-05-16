db9a851faca9af807231f082ab6ef143dc0af4ce
Walt Mankowski <waltman@pobox.com>
2019-05-16 16:21:29 -0400

    use command line parameters to set grid size

--- mazes/binary_tree_demo.py
+++ mazes/binary_tree_demo.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
+from sys import argv
 
 from Grid import Grid
 from Binary_Tree import Binary_Tree
 
-grid = Grid(5,5)
+rows = int(argv[1])
+cols = int(argv[2])
+
+grid = Grid(rows, cols)
 maze = Binary_Tree(grid)
 grid.show_grid()
